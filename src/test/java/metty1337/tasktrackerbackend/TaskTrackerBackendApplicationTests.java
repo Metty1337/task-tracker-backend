4c6a0eb6cd4448e00ff8ce31361d5ceabf724fe5
Metty1337 <c137morty3@gmail.com>
2026-09-06 23:43:32 +0300

    chore: init project

---
+++ src/test/java/metty1337/tasktrackerbackend/TaskTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package metty1337.tasktrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
